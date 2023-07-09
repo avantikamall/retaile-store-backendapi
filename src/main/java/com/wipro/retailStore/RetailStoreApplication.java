39e324bb98f1b2f81aa8cad229014301e980a17f
Avantika Mall <avantikamallvishen73@gmail.com>
2023-07-09 11:01:34 +0530

    first commit

---
+++ src/main/java/com/wipro/retailStore/RetailStoreApplication.java
@@ -0,0 +1,15 @@
+package com.wipro.retailStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailStoreApplication.class, args);
+		System.out.println("Hello!");
+		
+	}
+
+}
