e79ad9446998bd356cf5b158dda9f8cb3715c89f
Avantika Mall <avantikamallvishen73@gmail.com>
2023-07-11 00:13:20 +0530

    Services Class Added

--- src/main/java/com/wipro/retailStore/services/CustomerService.java
+++ src/main/java/com/wipro/retailStore/services/CustomerService.java
@@ -1,5 +1,16 @@
 package com.wipro.retailStore.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wipro.retailStore.repo.CustomerRepository;
+
+@Service
 public class CustomerService {
+	
+	@Autowired
+	CustomerRepository custrepo;
+	
+	
 
 }
